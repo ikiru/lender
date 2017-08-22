088a1a0b8c828cb17e451f87ad81badc60658cb1
Jeff Winkler <jeff.a.winkler@gmail.com>
2017-08-22 01:02:05 +0000

    parrallel logins

--- app/controllers/sessiona_controller.rb
+++ app/controllers/sessiona_controller.rb
@@ -1,20 +1,39 @@
 class SessionaController < ApplicationController
-  skip_before_action :require_login, only: [:create]
+  skip_before_action :require_login, only: [:createbank]
+  skip_before_action :require_login, only: [:createborrower]
 
-def create
-    user = User.find_by_email(params[:email]).try(:authenticate, params[:password])
-    if user
-      p user
-        session[:user_id] = user.id
-        return redirect_to "/dashboard"
+def createbank
+    bank = Bank.find_by_email(params[:email]).try(:authenticate, params[:password])
+    if bank
+      p bank
+        session[:bank_id] = bank.id
+        return redirect_to "/borrower"
     else
         flash[:errors] = ['Too Too Bad, So Sad. This Combo will not work']
         return redirect_to :back
     end
 end
 
-  def destroy
-      session[:user_id] = nil
+def createborrower
+    borrower = Borrower.find_by_email(params[:email]).try(:authenticate, params[:password])
+    if borrower
+      p borrower
+        session[:borrow_id] = borrow.id
+        return redirect_to "/borrower"
+    else
+        flash[:errors] = ['Too Too Bad, So Sad. This Combo will not work']
+        return redirect_to :back
+    end
+end
+
+  def destroybank
+      session[:bank_id] = nil
       return redirect_to '/'
   end
+
+  def destroyborrower
+      session[:borrower_id] = nil
+      return redirect_to '/'
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,10 @@ get 'borrower/:id' => 'borrowers#show'
 post 'bank' => 'banks#create'
 get 'bank/:id' => 'banks#show'
 
+post 'sessionsbank' => 'sessiona#createbank'
+delete 'sessionsbank/:id' => 'sessiona#destroybank'
+
+post 'sessionsborrower' => 'sessiona#createborrower'
+delete 'sessionsborrwer/:id' => 'sessiona#destroyborrower'
+
 end
